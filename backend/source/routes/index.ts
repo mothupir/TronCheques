d2510f1e39020095d046c5303ab874ad8e6407e1
mothupir <mothupir@bbd.co.za>
2024-04-18 09:58:48 +0200

    deleted

--- backend/source/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import withdraw from "./withdraw.route";
-
-const router = express.Router();
-
-router.use('/api', withdraw);
-
-export default router;
--- backend/source/routes/withdraw.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { withdraw } from "../controllers/withdraw.controller";
-
-const router = express.Router();
-
-router.use('/withdraw', withdraw);
-
-export default router;
