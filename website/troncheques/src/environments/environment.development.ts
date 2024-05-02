db8d4c98a79e75db010e74653f47de3097fed4d8
mothupir <mothupir@bbd.co.za>
2024-05-02 14:23:43 +0200

    addded environment

---
+++ website/troncheques/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    FULL_HOST: 'https://nile.trongrid.io',
+    DEPOSIT_ADDRESS: '41e77a5161441c536f8c51f1a03824cb26e8dc1fa2',
+    BASE_URL: 'http://localhost:3000/api/'
+};
