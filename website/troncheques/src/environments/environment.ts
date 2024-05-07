9fd3c229c393b8c1dfb5cb584e53623faf0ffc61
mothupir <mothupir@bbd.co.za>
2024-05-07 22:15:00 +0200

    .

--- website/troncheques/src/environments/environment.ts
+++ website/troncheques/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     FULL_HOST: 'https://nile.trongrid.io',
     DEPOSIT_ADDRESS: '41e77a5161441c536f8c51f1a03824cb26e8dc1fa2',
-    // BASE_URL: 'https://tron-cheques-api.vercel.app/api/',
-    BASE_URL: 'http://localhost:3000/api/',
+    BASE_URL: 'https://tron-cheques-api.vercel.app/api/',
+    // BASE_URL: 'http://localhost:3000/api/',
 };
